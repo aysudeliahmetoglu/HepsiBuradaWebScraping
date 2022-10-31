3195bec7f93c3985e80be0508faa721e51f2bc69
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-10-31 19:15:42 +0300

    web scraping continue

--- main.go
+++ main.go
@@ -1,47 +1,27 @@
 package main
 
-import (
+import(
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"net/http"
-	
-        
+
+	"github.com/gocolly/colly"
+
 )
 
+type item struct{
+	Name   string `json:"name"`
+	Price  string `json:"price"`
+	ImgUrl string  `json:"imgurl"`
+}
+
 func main() {
-	
-	// url := "https://www.hepsiburada.com/iphone-14-pro-max-512-gb-p-HBCV00002W46NM"
+	c := colly.NewCollector(
+		colly.AllowedDomains("https://www.hepsiburada.com"),
+
+	)
+	c.OnHTML("div[moria-ProductCard-joawUM cqPuXj sz7amsy3dwb]", func(h *colly.HTMLElement){
+		fmt.Println(h.Text)
+
+	})
+    c.Visit("https://www.hepsiburada.com/iphone-14-pro-max-c-80857057")
     
-	// res,_ := http.Get(url)
-
-	// if res.StatusCode != 200 {
-	// 	fmt.Println("Hata:",res.StatusCode)
-	// 	return
-	// }
-	client := &http.Client{
-		
-	}
-	// resp, err := http.Get("https://www.hepsiburada.com/iphone-14-pro-max-512-gb-p-HBCV00002W46NM")
-	
-	
-	req, err := http.NewRequest("GET", "https://www.hepsiburada.com/iphone-14-pro-max-512-gb-p-HBCV00002W46NM", nil)
-	
-
-	req.Header.Add("If-None-Match", `W/" Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"`)
-	
-	resp, err := client.Do(req)
-	fmt.Println(resp)
-	
-	if err != nil {
-		fmt.Println("error")
-	}
-	
-	
-	doc,_ := goquery.NewDocumentFromReader(resp.Body)
-
-
-    title := doc.Find(".productDetailRight .product-information").Text()	
-	price := doc.Find(".currentPriceBeforePoint").Text()
-    fmt.Println(title)
-	fmt.Println(price)
 }
